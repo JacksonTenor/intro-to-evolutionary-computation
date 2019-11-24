b9a8433894a121edb1962297a20bc4477341f16c
michael-small <small203@morris.umn.edu>
2019-11-24 16:38:55 -0600

    Push up in progress fix picking best child
    We found that the best score was not being picked from the frontier.
    
    This works as intended:
    
    intro-to-ec.2048.search=>
    (pm/priority-map [4 4 2 0 8 8 0 0 16 16 0 0 32 32 0 0] -854 [4 2 2 4 8 0 4 8 16 0 8 16 0 0 16 32] -480 [2 0 4 4 0 2 4 8 0 0 16 16 0 0 32 32] -720)
    
    {[4 4 2 0 8 8 0 0 16 16 0 0 32 32 0 0] -854,
     [2 0 4 4 0 2 4 8 0 0 16 16 0 0 32 32] -720,
     [4 2 2 4 8 0 4 8 16 0 8 16 0 0 16 32] -480}
    
    However, as you can see by one output from our actual output from the algorithm:
    
    {([[2 0 4 4 0 0 8 8 0 0 16 16 4 0 32 32] -756]
    [[4 2 4 4 0 0 8 8 0 0 16 16 0 0 32 32] -756]
    [[2 0 2 8 0 0 0 16 0 0 0 32 0 0 2 64] -1134]...)}
    
    The scores are not being put in order. We need a priority map to be applied somewhere.
    
    Provided that can be done in the right place (surely it will be obvious in retrospect),
    we should have the solutuion much sooner. Then we can work on a more optimized
    heuristic, one that considers more than just 0's
    
    Co-authored-by: UtkarshKr007 <kumar375@morris.umn.edu>

--- intro-to-ec/src/intro_to_ec/2048/search.clj
+++ intro-to-ec/src/intro_to_ec/2048/search.clj
@@ -1,5 +1,5 @@
 (ns intro-to-ec.2048.search
-  (:require [intro-to-ec.2048.game :refer [board-left board-right board-up board-down start-board]])
+  (:require [intro-to-ec.2048.game :refer [board-left board-right board-up board-down start-board print-board]])
   (:require [clojure.set :as cset])
   (:require [clojure.data.priority-map :as pm]))
 
@@ -7,6 +7,26 @@
 (defn has-2048?
   [board]
   (not= (.indexOf board 2048) -1))
+
+(defn has-512?
+  [board]
+  (not= (.indexOf board 512) -1))
+
+(defn has-256?
+  [board]
+  (not= (.indexOf board 256) -1))
+
+(defn has-64?
+  [board]
+  (not= (.indexOf board 64) -1))
+
+(defn has-16?
+  [board]
+  (not= (.indexOf board 16) -1))
+
+(defn has-8?
+  [board]
+  (not= (.indexOf board 8) -1))
 
 "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~heuristic~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 (defn score
@@ -20,6 +40,16 @@
 (defn score-times-zeros
   [board]
   (*(score board) (count-zeros board)))
+
+; One thing we tried. Had the same amount of steps, just a different score.
+(defn score-board
+  [board]
+  (score board))
+
+; One thing we tried. Had the same amount of steps, just a different score.
+(defn score-times-zeros-retain-some-multiplier
+  [board]
+  (*(score board) (+ 0.1 (count-zeros board))))
 
 "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Stolen~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 (defn remove-visited
@@ -43,11 +73,15 @@
   [kids frontier]
   (into frontier (map (fn [kid] [kid (* -1 (score-times-zeros kid))]) kids)))
 
+(defn add-children-modified
+  [kids frontier]
+  (assoc frontier  (fn [kid] [kid (* -1 (score-times-zeros kid))]) kids) )
+
 (def the-magic-question
   {:get-next (fn [pmap] (first (first pmap)))
    :add-children add-children})
 (def the-magic-solution
-  {:goal? has-2048?
+  {:goal? has-64?
    :make-children make-children
    })
 
@@ -58,7 +92,11 @@
   (loop [frontier (pm/priority-map start-state 10)
          came-from {start-state :start}
          num-calls 0]
-    (println num-calls ": " (first frontier))
+    (println num-calls ": " (first frontier) "\n")
+    ; (println "\n" num-calls "\n")
+    ; (println (pm/priority-map frontier 10))
+    (println "\nRest: " "\n\n" (rest frontier) "\n")
+    (println "~~~~~~~~~~~~~~~~~")
     (let [current (get-next frontier)]
       (cond
         (goal? current) (generate-path came-from current)
